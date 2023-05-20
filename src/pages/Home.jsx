3eff5c76c8315a46bbcf335846d8836d1a96f729
Jhoneshark <jhonesharky@gmail.com>
2023-05-20 17:55:47 -0300

    init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+import MovieCard from "../components/MovieCard";
+import "./MoviesGrid.css";
+
+const moviesURL = import.meta.env.VITE_API;
+const apikey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const [topMovies, setTopMovies] = useState([]);
+
+  const getTopRateMovies = async (url) => {
+    const res = await fetch(url);
+    const data = await res.json();
+
+    setTopMovies(data.results);
+  };
+
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apikey}`;
+
+    getTopRateMovies(topRatedUrl);
+  }, []);
+
+  return (
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
+      <div className="movies-container">
+        {topMovies === 0 && <p>CARREGANDO....!!!!</p>}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
